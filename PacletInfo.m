82b24aa8c542b9540eeb27c7193d1080d0e51eaf
flip phillips <flip@skidmore.edu>
2018-01-29 20:08:03 -0500

    added paclet

---
+++ PacletInfo.m
@@ -0,0 +1,13 @@
+Paclet[
+  Name -> "Fuzzy",
+  Description -> "Simple fuzzy logic operations for simple folk.",
+  Creator -> "Flip Phillips",
+  Publisher -> "Skidmore College Vision Labs",
+  Version -> "0.0.2",
+  MathematicaVersion -> "7+",
+  Loading -> Automatic,
+  Extensions -> {
+   { "Documentation", Language -> "English"},
+   { "Application", Context -> "Fuzzy`" }
+  }
+]
